50550161ab57d641cf0c9003af9a3d50b667ba9a
Alucard <xavier_ortega98@hotmail.com>
2021-08-22 23:34:51 -0500

    Grafica lista

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,14 +4,14 @@ import CountUp from 'react-countup'
 import styles from './Cards.module.css';
 
 const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
-    console.log(confirmed, recovered, deaths, lastUpdate);
+    // console.log(confirmed, recovered, deaths, lastUpdate);
     if (!confirmed) {
         return 'Loading...'
     }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justifyContent="center">
-                <Grid item component={Card} className={styles.card && styles.infected} sm={12} md={3}>
+                <Grid item component={Card} className={styles.card+ ' ' + styles.infected} sm={12} md={3}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -21,7 +21,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant="body2">Number of active cases</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} className={styles.card && styles.recovered} sm={12} md={3}>
+                <Grid item component={Card} className={styles.card+ ' ' + styles.recovered} sm={12} md={3}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -31,7 +31,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant="body2">Number of recoveries from covid 19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card} className={styles.card && styles.deaths} sm={12} md={3}>
+                <Grid item component={Card} className={styles.card+ ' ' + styles.deaths} sm={12} md={3}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchDailyData } from '../../api';
+import { Line } from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetching = async () => {
+            const initialDailyData = await fetchDailyData();
+
+            setDailyData(initialDailyData);
+        };
+
+        fetching();
+    }, [])
+
+    const lineChart = (
+        dailyData.length ? ( 
+            <Line
+            data={{
+                labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+                datasets: [{
+                  data: dailyData.map((data) => data.confirmed),
+                  label: 'Infectados',
+                  borderColor: '#3333ff',
+                  fill: true,
+                }, {
+                  data: dailyData.map((data) => data.deaths),
+                  label: 'Muertos',
+                  borderColor: 'red',
+                  backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                  fill: true,
+                },
+                ],
+              }}
+            /> 
+        ): null
+    );
+
     return (
-        <div>
-           <h1>Chart</h1> 
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
